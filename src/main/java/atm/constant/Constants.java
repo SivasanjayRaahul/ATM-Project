7160b24699a8bfa686ec6ab4aacf1a73db126b50
sruthiar <sruthi.ar@thoughtworks.com>
2022-07-12 20:25:03 +0530

    [Sivasanjay | Sruthi] Add Bank Initial Authentication

--- src/main/java/atm/constant/Constants.java
+++ src/main/java/atm/constant/Constants.java
@@ -1,5 +1,8 @@
 package atm.constant;
 
+import java.util.Scanner;
+
 public class Constants {
     public static String mySqlDriver ="com.mysql.cj.jdbc.Driver";
+    public static Scanner scanner = new Scanner(System.in);
 }
