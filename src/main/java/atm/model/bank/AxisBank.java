db4b6ab5f43d68b2ef879553175c19bf09863886
sruthiar <sruthi.ar@thoughtworks.com>
2022-07-20 16:55:29 +0530

    [Sruthi | Sivasanjay] Refactor Constants

--- src/main/java/atm/model/bank/AxisBank.java
+++ src/main/java/atm/model/bank/AxisBank.java
@@ -4,10 +4,12 @@ import atm.constant.Constants;
 
 public class AxisBank implements Bank {
 
+    @Override
     public String getName() {
         return Constants.AXIS_NAME;
     }
 
+    @Override
     public String getIFSC() {
         return Constants.AXIS_IFSC;
     }
