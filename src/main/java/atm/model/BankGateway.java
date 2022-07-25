17177eef03a11de5dc140f1c48b835912a87839a
SivasanjayRaahul <sivasanjay.mohan@thoughtworks.com>
2022-07-25 15:35:08 +0530

    [Sivasanjay] Refactor crearting user and bank account

--- src/main/java/atm/model/BankGateway.java
+++ src/main/java/atm/model/BankGateway.java
@@ -1,5 +1,6 @@
 package atm.model;
 
+import atm.model.bank.AxisBank;
 import atm.model.bank.Bank;
 import atm.model.bank.HDFCBank;
 
@@ -22,9 +23,9 @@ public class BankGateway {
             user.createAccount(bank, accountType);
     }
 
-    public User getUser(String customerId){
-        for(User user:this.users){
-            if(user.getCustomerId().equals(customerId))
+    public User getUser(String customerId) {
+        for (User user : this.users) {
+            if (user.getCustomerId().equals(customerId))
                 return user;
         }
         return null;
@@ -32,10 +33,47 @@ public class BankGateway {
 
     public static void main(String[] args) {
         BankGateway bankGateway = new BankGateway();
-        String customerId = bankGateway.createUser("sru", "sru@gmail.com", 21);
-        String customerIdForBank = SCANNER.next();
-        User user = bankGateway.getUser(customerIdForBank);
-        bankGateway.createBankAccount(user,new HDFCBank(),"Savings");
+
+        System.out.println("1)Create an account");
+        System.out.println("2)Create a bank account");
+        System.out.println("3)Create ATM card");
+        int choice = SCANNER.nextInt();
+        if (choice == 1) {
+
+            System.out.println("Enter the name");
+            String name = SCANNER.next();
+
+            System.out.println("Enter the mail address");
+            String mail = SCANNER.next();
+
+            System.out.println("Enter the age");
+            int age = SCANNER.nextInt();
+            System.out.println("Your Customer Id is" + bankGateway.createUser(name, mail, age));
+        } else if (choice == 2) {
+            System.out.println("Enter the customerId");
+            String customerId = SCANNER.next();
+
+            User user = bankGateway.getUser(customerId);
+            System.out.println("Select Bank name:\n1)HDFC\n2)AXIS");
+            Bank bank;
+            int bankChoice = SCANNER.nextInt();
+            if (bankChoice == 1)
+                bank = new HDFCBank();
+            else
+                bank = new AxisBank();
+
+
+            System.out.println("Select Account Type:\n1)Savings\n2)Current");
+            String accountType;
+            int accountTypeChoice = SCANNER.nextInt();
+            if (accountTypeChoice == 1)
+                accountType = "Savings";
+            else
+                accountType = "Current";
+
+            bankGateway.createBankAccount(user, bank, accountType);
+        }
+
     }
 
 }
