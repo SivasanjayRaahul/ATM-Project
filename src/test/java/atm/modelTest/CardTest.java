6b6098ddad5ab18a6b225e45c7863ae9bbe0c439
sruthiar <sruthi.ar@thoughtworks.com>
2022-07-27 20:34:38 +0530

    [Sruthi | Sivasanjay] Add Testcase for CardTest

---
+++ src/test/java/atm/modelTest/CardTest.java
@@ -0,0 +1,47 @@
+package atm.modelTest;
+
+import atm.model.Account;
+import atm.model.Card;
+import atm.model.User;
+import atm.model.bank.HDFCBank;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CardTest {
+    static Account account;
+    static Card card;
+
+    @BeforeAll
+    static void beforeAll() {
+        User user = new User("Siva", "ssr@gmail.com", 21);
+        String customerId = user.getCustomerId();
+        user.createAccount(new HDFCBank(), "SAVINGS");
+        List<Account> bankAccounts = user.getBankAccounts(customerId);
+        account = bankAccounts.get(0);
+        account.createCard(1234);
+        card = account.getCard();
+    }
+
+    @Test
+    void shouldReturnSameBalanceAsInAccountWhenCreatingCardInitially() {
+
+        assertEquals(account.checkBalance(), card.checkBalance());
+    }
+
+    @Test
+    void shouldReturnTrueAsCardStatusWhenCardIsUnblock() {
+        assertTrue(card.isStatus());
+    }
+
+    @Test
+    void shouldReturnFalseAsCardStatusWhenCardIsBlock() {
+        card.block();
+        assertFalse(card.isStatus());
+    }
+
+}
